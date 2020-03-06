d46132846d38d2abd17c6d4b09db6ec0c0d32c35
zhazhalaila <1176694275@qq.com>
2020-03-06 15:22:11 +0800

    add web form

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_admin import Admin
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
 from app import routes
