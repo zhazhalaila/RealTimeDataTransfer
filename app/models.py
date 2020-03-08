2e2ec79e863323df235c09988232aa471bf1b589
zhazhalaila <1176694275@qq.com>
2020-03-08 20:14:09 +0800

    add login function

--- app/models.py
+++ app/models.py
@@ -1,14 +1,27 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    mqtt_topic = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     sensors = db.relationship('Sensor', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
