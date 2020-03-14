115165ebaa35afe5b497d02db4823fc0d8a81a08
zhazhalaila <1176694275@qq.com>
2020-03-14 17:55:37 +0800

    test for dynamic wifi connect

--- arduino/AsyncServer/AsyncServer.ino
+++ arduino/AsyncServer/AsyncServer.ino
@@ -1,23 +1,31 @@
 #include "WiFi.h"
 #include "ESPAsyncWebServer.h"
  
-const char *ssid = "MyESP32AP";
-const char *password = "testpassword";
- 
+const char *ssid = "cainiaoa";
+const char *password = "66666666";
+const char* assid = "esp32";
+const char* asecret = "12345678";
+
 AsyncWebServer server(80);
  
 void setup(){
   Serial.begin(115200);
 
+  //ap and station open at the same time
   WiFi.mode(WIFI_AP_STA);
- 
-  WiFi.softAP(ssid, password);
   
-  Serial.println();
-  Serial.print("IP address: ");
+  Serial.println("Creating Accesspoint");
+  WiFi.softAP(assid,asecret,7,0,5);
+  Serial.print("IP address:\t");
   Serial.println(WiFi.softAPIP());
- 
+
+  //code for test wifi dynamic config
   server.on("/hello", HTTP_GET, [](AsyncWebServerRequest *request){
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
     request->send(200, "text/plain", "Hello World");
   });
  
